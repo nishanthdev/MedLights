347dfae607da58465a006dc41e1affe8f514ed2e
Manjunath <manjunath.shenoy4@gmail.com>
2018-03-18 19:51:40 +0530

    Fixed message module

--- doctor_list.php
+++ doctor_list.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include "./database/DB.php";
 
  ?>
@@ -29,7 +29,7 @@ include "./database/DB.php";
 <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 </head>
 <body class="cnt-home">
-<?php include "./head.php"; 
+<?php include "./head.php";
 
 // session_start();
 if(!isset($_SESSION['state']))
@@ -43,10 +43,10 @@ if(!isset($_SESSION['state']))
 <!-- ============================================== HEADER ============================================== -->
 <div class="body-content outer-top-xs" id="top-banner-and-menu">
   <div class="container">
-    <div class="row"> 
+    <div class="row">
       <!-- ============================================== SIDEBAR ============================================== -->
-      <div class="col-xs-12 col-sm-12 col-md-3 sidebar"> 
-        
+      <div class="col-xs-12 col-sm-12 col-md-3 sidebar">
+
         <!-- ================================== TOP NAVIGATION ================================== -->
         <div class="side-menu animate-dropdown outer-bottom-xs">
           <div class="head"><i class="icon fa fa-align-justify fa-fw"></i>Recent Chats</div>
@@ -66,7 +66,7 @@ if(!isset($_SESSION['state']))
                       <label>To:</label>
                       <select class="form-control" name="reciever">
                        <?php $d = "select name, user_id from user where type ='doctor'";
-                        $res = $link->query($d); 
+                        $res = $link->query($d);
                          while ($row = mysqli_fetch_row($res))
                          {
                         ?>
@@ -86,46 +86,46 @@ if(!isset($_SESSION['state']))
               </div>
             </div>
       <!-- <ul class="list-group"> -->
-        <?php 
-      $a = "select DISTINCT message.reciever, user.username as uname, user.user_id as uid, user.pic as pic from user, message where message.reciever = user.user_id and sender = '$id'";
-       
-        $res = $link->query($a);
-        while ($row = mysqli_fetch_assoc($res)) {
-                
+        <?php
+      $a = "SELECT * FROM user WHERE type='doctor'";
+
+        $doctors = $link->query($a);
+        foreach ($doctors as $key => $doctor){
+
         ?>
-        <li class="dropdown menu-item"><a href="doctor_list.php?id=<?php echo $row['uid']; ?>"><?php echo $row['uname']; ?></a></li>
-              <?php } 
-            ?>  
+        <li class="dropdown menu-item"><a href="doctor_list.php?id=<?php echo $doctor['user_id']; ?>"><?php echo $doctor['name']; ?></a></li>
+              <?php }
+            ?>
               </ul>
             </ul>
-            <!-- /.nav --> 
+            <!-- /.nav -->
           </nav>
-          <!-- /.megamenu-horizontal --> 
+          <!-- /.megamenu-horizontal -->
         </div>
-        <!-- /.side-menu --> 
+        <!-- /.side-menu -->
         <!-- ============================================== SPECIAL OFFER ============================================== -->
-       
+
         <!-- <div class="home-banner"> <img src="assets/images/banners/LHS-banner.jpg" alt="Image"> </div> -->
       </div>
-      <!-- /.sidemenu-holder --> 
-      <!-- ============================================== SIDEBAR : END ============================================== --> 
-      
+      <!-- /.sidemenu-holder -->
+      <!-- ============================================== SIDEBAR : END ============================================== -->
+
       <!-- ============================================== CONTENT ============================================== -->
-   <div class="col-xs-12 col-sm-12 col-md-9 homebanner-holder"> 
+   <div class="col-xs-12 col-sm-12 col-md-9 homebanner-holder">
         <!-- ========================================== SECTION – HERO ========================================= -->
         <div class="panel" style="padding: 10px;">
-        	
-          <?php if (!isset($_GET['id'])) { 
+
+          <?php if (!isset($_GET['id'])) {
 
 
             ?>
           <div>
-			     <h1 class="display-3 text-center">No Messages</h1>        		
+			     <h1 class="display-3 text-center">No Messages</h1>
         	</div>
-        </div>  
+        </div>
         <?php } else { ?>
         <div>
-          <?php 
+          <?php
           $mid = $_GET['id'];
           $uname = $_SESSION['username'];
           $m = "SELECT * FROM `user` where username = '" . $uname . "'";
@@ -133,26 +133,26 @@ if(!isset($_SESSION['state']))
           while ($row = mysqli_fetch_row($res)) {
           $uid = $row[0];
           } ?>
-          <?php 
+          <?php
           $m1 = "select body,sender,reciever,sent_at from message where sender = '$uid' and reciever = '$mid' or reciever = '$uid' and sender = '$mid' ORDER BY sent_at ASC";
            $res = $link->query($m1);
            while($row = mysqli_fetch_assoc($res)) {
-           
+
            if ($uid ==$row['sender']) {
            ?>
            <div class="well" style="background-color: lightblue; border-radius: 1em; color: white;margin-left: 50%;text-align: right;">
             <h4><?php echo $row['body']; ?></h4>
             <p style="float: left;"><?php echo $row['sent_at']; ?></p>
           </div>
-        <?php 
-          } 
+        <?php
+          }
 
           else {
             ?>
           <div class="well"  style="background-color: lightgreen; border-radius: 1em; color: white; margin-right: 50%;text-align: left;">
             <h4><?php echo $row['body']; ?></h4>
             <p style="float: right;"><?php echo $row['sent_at']; ?></p>
-          </div>    
+          </div>
             <?php
           }
       }
@@ -170,17 +170,17 @@ if(!isset($_SESSION['state']))
       } ?>
       </div>
       </div>
-      <!-- /.homebanner-holder --> 
-      <!-- ============================================== CONTENT : END ============================================== --> 
+      <!-- /.homebanner-holder -->
+      <!-- ============================================== CONTENT : END ============================================== -->
     </div>
-    
+
   </div>
-  <!-- /.container --> 
+  <!-- /.container -->
 
 
 <!--  -->
 </div>
-<!-- /#top-banner-and-menu --> 
+<!-- /#top-banner-and-menu -->
 
 <!-- ============================================================= FOOTER ============================================================= -->
 <footer id="footer" class="footer color-bg">
@@ -192,7 +192,7 @@ if(!isset($_SESSION['state']))
             <h4 class="module-title">Contact Us</h4>
           </div>
           <!-- /.module-heading -->
-          
+
           <div class="module-body">
             <ul class="toggle-footer" style="">
               <li class="media">
@@ -214,16 +214,16 @@ if(!isset($_SESSION['state']))
               </li>
             </ul>
           </div>
-          <!-- /.module-body --> 
+          <!-- /.module-body -->
         </div>
         <!-- /.col -->
-        
+
         <div class="col-xs-12 col-sm-6 col-md-3">
           <div class="module-heading">
             <h4 class="module-title">Customer Service</h4>
           </div>
           <!-- /.module-heading -->
-          
+
           <div class="module-body">
             <ul class='list-unstyled'>
               <li class="first"><a href="#" title="Contact us">My Account</a></li>
@@ -233,16 +233,16 @@ if(!isset($_SESSION['state']))
               <li class="last"><a href="#" title="Where is my order?">Help Center</a></li>
             </ul>
           </div>
-          <!-- /.module-body --> 
+          <!-- /.module-body -->
         </div>
         <!-- /.col -->
-        
+
         <div class="col-xs-12 col-sm-6 col-md-3">
           <div class="module-heading">
             <h4 class="module-title">Corporation</h4>
           </div>
           <!-- /.module-heading -->
-          
+
           <div class="module-body">
             <ul class='list-unstyled'>
               <li class="first"><a title="Your Account" href="#">About us</a></li>
@@ -252,16 +252,16 @@ if(!isset($_SESSION['state']))
               <li class="last"><a title="Orders History" href="#">Advanced Search</a></li>
             </ul>
           </div>
-          <!-- /.module-body --> 
+          <!-- /.module-body -->
         </div>
         <!-- /.col -->
-        
+
         <div class="col-xs-12 col-sm-6 col-md-3">
           <div class="module-heading">
             <h4 class="module-title">Why Choose Us</h4>
           </div>
           <!-- /.module-heading -->
-          
+
           <div class="module-body">
             <ul class='list-unstyled'>
               <li class="first"><a href="#" title="About us">Shopping Guide</a></li>
@@ -271,7 +271,7 @@ if(!isset($_SESSION['state']))
               <li class=" last"><a href="contact-us.html" title="Suppliers">Contact Us</a></li>
             </ul>
           </div>
-          <!-- /.module-body --> 
+          <!-- /.module-body -->
         </div>
       </div>
     </div>
@@ -289,29 +289,29 @@ if(!isset($_SESSION['state']))
             <li><img src="assets/images/payments/5.png" alt=""></li>
           </ul>
         </div>
-        <!-- /.payment-methods --> 
+        <!-- /.payment-methods -->
       </div>
     </div>
   </div>
 </footer>
-<!-- ============================================================= FOOTER : END============================================================= --> 
+<!-- ============================================================= FOOTER : END============================================================= -->
 
-<!-- For demo purposes – can be removed on production --> 
+<!-- For demo purposes – can be removed on production -->
 
-<!-- For demo purposes – can be removed on production : End --> 
+<!-- For demo purposes – can be removed on production : End -->
 
-<!-- JavaScripts placed at the end of the document so the pages load faster --> 
-<script src="assets/js/jquery-1.11.1.min.js"></script> 
-<script src="assets/js/bootstrap.min.js"></script> 
-<script src="assets/js/bootstrap-hover-dropdown.min.js"></script> 
-<script src="assets/js/owl.carousel.min.js"></script> 
-<script src="assets/js/echo.min.js"></script> 
-<script src="assets/js/jquery.easing-1.3.min.js"></script> 
-<script src="assets/js/bootstrap-slider.min.js"></script> 
-<script src="assets/js/jquery.rateit.min.js"></script> 
-<script type="text/javascript" src="assets/js/lightbox.min.js"></script> 
-<script src="assets/js/bootstrap-select.min.js"></script> 
-<script src="assets/js/wow.min.js"></script> 
+<!-- JavaScripts placed at the end of the document so the pages load faster -->
+<script src="assets/js/jquery-1.11.1.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/bootstrap-hover-dropdown.min.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/echo.min.js"></script>
+<script src="assets/js/jquery.easing-1.3.min.js"></script>
+<script src="assets/js/bootstrap-slider.min.js"></script>
+<script src="assets/js/jquery.rateit.min.js"></script>
+<script type="text/javascript" src="assets/js/lightbox.min.js"></script>
+<script src="assets/js/bootstrap-select.min.js"></script>
+<script src="assets/js/wow.min.js"></script>
 <script src="assets/js/scripts.js"></script>
 </body>
 </html>
